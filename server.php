2861343d9c167f76af14ca271cc90cd712d62456
Vitali Z <vitalizybailo@gmail.com>
2018-11-19 21:26:59 +0300

    formms

--- server.php
+++ server.php
@@ -1,5 +1,24 @@
 <?php 
 
-    echo "работаааааает"
+    $name = $_POST['name'];
+    $link = $_POST['link'];
+    $text = $_POST['text'];
 
+    $mail_message = '
+    <html>
+        <head>
+            <title>Заявка</title>
+        </head>
+        <body>
+            <h2>Заказ</h2>
+            <ul>
+                <li>Имя: ' . $name . '</li>
+                <li>Ссылка на страницу: ' . $link . '</li>
+                <li>Комментарии к заказу: ' . $text . '</li>
+            </ul>
+        </body>
+    </html>    
+    ';
+
+    echo  $mail_message;
 ?>
